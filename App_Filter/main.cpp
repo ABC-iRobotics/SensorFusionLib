81d1aac067e9cb53891588885a8e05fa2334c1db
kj <kutij6@gmail.com>
2019-07-01 11:51:35 +0200

    Plotter application

--- App_Filter/main.cpp
+++ App_Filter/main.cpp
@@ -1,8 +1,7 @@
 #include "youBotINSGPS.h"
 
 int main(void) {
-	youBotINSGPS filter(5555);
-
+	youBotINSGPS filter(5555, 5556);
 	filter.run();
 
 	return 0;
--- App_Plotter/main.cpp
+++ App_Plotter/main.cpp
@@ -1,5 +1,31 @@
 
+#include "ZMQSubscriber.h"
+#include "FilterPlot.h"
+#include "youBotSystem.h"
+#include "INSSensor.h"
 
 int main(void) {
-	
+	int youbotID = 0;
+	int insID = 2;
+	int gpsID = 1;
+
+	youBotSystem::BaseSystemPtr youBot = std::make_shared<youBotSystem>(0, 0, 0, 0, youbotID);
+	INSSensor::SensorPtr ins = std::make_shared<INSSensor>(youBot, insID);
+
+	FilterPlot plotter(youbotID, youBot->getName(), youBot->getStateNames(), STATE);
+	//FilterPlot plotter2(youbotID, youBot->getName(), youBot->getDisturbanceNames(), DISTURBANCE);
+	//FilterPlot plotter3(insID, ins->getName(), ins->getOutputNames(), OUTPUT);
+
+
+	ZMQSubscriber sub(5556);
+
+	while (true) {
+		DataMsg data;
+		bool success = sub.RecvMsg_Wait(data);
+		while (success) {
+			FilterPlot::AddData(data);
+			success = sub.RecvMsg_DontWait(data);
+		}
+		FilterPlot::UpdateWindows();
+	}
 }
