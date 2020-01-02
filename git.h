216d86c1e4b90fc53a630a15aa645d9a4500af1a
kj <kutij6@gmail.com>
2020-01-02 22:47:31 +0100

    a

---
+++ git.h
@@ -0,0 +1,11 @@
+// This file was create automatically by CMake.
+#pragma once
+
+// Whether or not we retrieved the state of the repo.
+#define GIT_RETRIEVED_STATE true
+
+// The SHA1 for the HEAD of the repo.
+#define GIT_HEAD_SHA1 "afa0ae071b82a9cb83a7ff14b8e98dc0e3871c8a"
+
+// Whether or not there were uncommited changes present.
+#define GIT_IS_DIRTY true
