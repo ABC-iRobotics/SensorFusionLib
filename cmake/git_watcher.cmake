49fea2a21d08c34d26f967eb4efe72afe2fd8191
kj <kutij6@gmail.com>
2020-01-03 14:23:23 +0100

    Cleaning versioning

--- cmake/git_watcher.cmake
+++ cmake/git_watcher.cmake
@@ -1,45 +1,8 @@
 # git_watcher.cmake
 #
 # License: MIT
-# Source: https://raw.githubusercontent.com/andrew-hardin/cmake-git-version-tracking/master/git_watcher.cmake
+# Based on: https://raw.githubusercontent.com/andrew-hardin/cmake-git-version-tracking/master/git_watcher.cmake
 
-
-# This file defines the functions and targets needed to monitor
-# the state of a git repo. If the state changes (e.g. a commit is made),
-# then a file gets reconfigured.
-#
-# The behavior of this script can be modified by defining any of these variables:
-#
-#   PRE_CONFIGURE_FILE (REQUIRED)
-#   -- The path to the file that'll be configured.
-#
-#   POST_CONFIGURE_FILE (REQUIRED)
-#   -- The path to the configured PRE_CONFIGURE_FILE.
-#
-#   GIT_STATE_FILE (OPTIONAL)
-#   -- The path to the file used to store the previous build's git state.
-#      Defaults to the current binary directory.
-#
-#   GIT_WORKING_DIR (OPTIONAL)
-#   -- The directory from which git commands will be run.
-#      Defaults to the directory with the top level CMakeLists.txt.
-#
-#   GIT_EXECUTABLE (OPTIONAL)
-#   -- The path to the git executable. It'll automatically be set if the
-#      user doesn't supply a path.
-#
-# Script design:
-#   - This script was designed similar to a Python application
-#     with a Main() function. I wanted to keep it compact to
-#     simplify "copy + paste" usage.
-#
-#   - This script is made to operate in two CMake contexts:
-#       1. Configure time context (when build files are created).
-#       2. Build time context (called via CMake -P)
-#     If you see something odd (e.g. the NOT DEFINED clauses),
-#     consider that it can run in one of two contexts.
-
-# Short hand for converting paths to absolute.
 macro(PATH_TO_ABSOLUTE var_name)
     get_filename_component(${var_name} "${${var_name}}" ABSOLUTE)
 endmacro()
@@ -60,8 +23,6 @@ macro(CHECK_OPTIONAL_VARIABLE var_name default_value)
     PATH_TO_ABSOLUTE(${var_name})
 endmacro()
 
-CHECK_REQUIRED_VARIABLE(PRE_CONFIGURE_FILE)
-CHECK_REQUIRED_VARIABLE(POST_CONFIGURE_FILE)
 CHECK_OPTIONAL_VARIABLE(GIT_STATE_FILE "${CMAKE_BINARY_DIR}/versioning/git-state")
 CHECK_OPTIONAL_VARIABLE(GIT_WORKING_DIR "${CMAKE_SOURCE_DIR}")
 
@@ -74,16 +35,6 @@ CHECK_REQUIRED_VARIABLE(GIT_EXECUTABLE)
 
 
 
-# Function: GitStateChangedAction
-# Description: this function is executed when the state of the git
-#              repo changes (e.g. a commit is made).
-function(GitStateChangedAction _state_as_list)
-    # Set variables by index, then configure the file w/ these variables defined.
-    LIST(GET _state_as_list 0 GIT_RETRIEVED_STATE)
-    LIST(GET _state_as_list 1 GIT_HEAD_SHA1)
-    LIST(GET _state_as_list 2 GIT_IS_DIRTY)
-    configure_file("${PRE_CONFIGURE_FILE}" "${POST_CONFIGURE_FILE}" @ONLY)
-endfunction()
 
 
 
@@ -144,29 +95,38 @@ function(GetGitState _working_dir _state)
     set(${_state} ${_success} ${_hashvar} ${_dirty} ${_version} PARENT_SCOPE)
 endfunction()
 
-
-
-# Function: CheckGit
-# Description: check if the git repo has changed. If so, update the state file.
-# Args:
-#   _working_dir    (in)  string; the directory from which git commands will be ran.
-#   _state_changed (out)    bool; whether or no the state of the repo has changed.
-#   _state         (out)    list; the repository state as a list (e.g. commit SHA).
-function(CheckGit _working_dir _state_changed _state)
-
+if(NOT DEFINED _BUILD_TIME_CHECK_GIT)
+	# Scope of the original cmake script at compiler stage:
+	# this function sets up custom commands that make the build system
+	# check the state of git before every build. If the state has
+	# changed, then a file is configured.
+	set(PRE_CONFIGURE_FILE "${CMAKE_SOURCE_DIR}/cmake/sf_version.h.in")
+	set(POST_CONFIGURE_FILE "${CMAKE_BINARY_DIR}/versioning/sf_version.h")
+	add_custom_target(check_git_repository
+	ALL
+	DEPENDS ${PRE_CONFIGURE_FILE}
+	BYPRODUCTS ${POST_CONFIGURE_FILE}
+	COMMENT "Checking the git repository for changes..."
+	COMMAND
+		${CMAKE_COMMAND}
+		-D_BUILD_TIME_CHECK_GIT=TRUE
+		-DGIT_WORKING_DIR=${GIT_WORKING_DIR}
+		-DGIT_EXECUTABLE=${GIT_EXECUTABLE}
+		-DGIT_STATE_FILE=${GIT_STATE_FILE}
+		-DPRE_CONFIGURE_FILE=${PRE_CONFIGURE_FILE}
+		-DPOST_CONFIGURE_FILE=${POST_CONFIGURE_FILE}
+		-DORIGINAL_SOURCE_FOLDER=${CMAKE_SOURCE_DIR}
+		-P "${CMAKE_CURRENT_LIST_FILE}")
+else()
+	# Check if the repo has changed. If so, run the change action.
     # Get the current state of the repo.
-    GetGitState("${_working_dir}" state)
-
-    # Set the output _state variable.
-    # (Passing by reference in CMake is awkward...)
-    set(${_state} ${state} PARENT_SCOPE)
+    GetGitState("${GIT_WORKING_DIR}" state)
 
     # Check if the state has changed compared to the backup on disk.
     if(EXISTS "${GIT_STATE_FILE}")
         file(READ "${GIT_STATE_FILE}" OLD_HEAD_CONTENTS)
         if(OLD_HEAD_CONTENTS STREQUAL "${state}")
             # State didn't change.
-            set(${_state_changed} "false" PARENT_SCOPE)
             return()
         endif()
     endif()
@@ -175,83 +135,30 @@ function(CheckGit _working_dir _state_changed _state)
     # We need to update the state file on disk.
     # Future builds will compare their state to this file.
     file(WRITE "${GIT_STATE_FILE}" "${state}")
+	
 	# Create SF_VERSION_FULL
 	LIST(GET state 3 SF_VERSION_FULL)
 	string(REGEX REPLACE "^([0-9]+)\\..*" "\\1" GIT_VERSION_MAJOR "${SF_VERSION_FULL}")
 	string(REGEX REPLACE "^[0-9]+\\.([0-9]+)\\..*" "\\1" GIT_VERSION_MINOR "${SF_VERSION_FULL}")
 	string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+)-?.*" "\\1" GIT_VERSION_PATCH "${SF_VERSION_FULL}")
-	configure_file("${ORIGINAL_SOURCE_FOLDER}/cmake/SensorFusionConfigVersion.cmake.in"
-					"${CMAKE_BINARY_DIR}/versioning/SensorFusionConfigVersion.cmake" @ONLY)
+	string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.[0-9]+-?([a-zA-Z0-9]*-?[a-zA-Z0-9]*)$" "\\1" GIT_VERSION_COMMIT "${SF_VERSION_FULL}")
 	LIST(GET state 2 GIT_IS_DIRTY)
 	if (GIT_IS_DIRTY)
 		set(SF_VERSION_FULL "${SF_VERSION_FULL}-dirty")
 	endif()
+	
 	# Create versioning/ConfigVersionCheck.cmake
 	configure_file("${ORIGINAL_SOURCE_FOLDER}/cmake/ConfigVersionCheck.cmake.in" "${CMAKE_BINARY_DIR}/versioning/ConfigVersionCheck.cmake" @ONLY)
+	configure_file("${ORIGINAL_SOURCE_FOLDER}/cmake/SensorFusionConfigVersion.cmake.in"
+					"${CMAKE_BINARY_DIR}/versioning/SensorFusionConfigVersion.cmake" @ONLY)
+					
 	# Create versioning/ConfigVersionCheck.cmake
 	file(WRITE "${CMAKE_BINARY_DIR}/versioning/Version.cmake" "set(SF_VERSION_FULL \"${SF_VERSION_FULL}\")")
-    set(${_state_changed} "true" PARENT_SCOPE)
-endfunction()
 
+	# Set variables by index, then configure the file w/ these variables defined.
+	LIST(GET state 0 GIT_RETRIEVED_STATE)
+	LIST(GET state 1 GIT_HEAD_SHA1)
+	LIST(GET state 2 GIT_IS_DIRTY)
+	configure_file("${PRE_CONFIGURE_FILE}" "${POST_CONFIGURE_FILE}" @ONLY)
+endif()
 
-
-# Function: SetupGitMonitoring
-# Description: this function sets up custom commands that make the build system
-#              check the state of git before every build. If the state has
-#              changed, then a file is configured.
-function(SetupGitMonitoring)
-    add_custom_target(check_git_repository
-        ALL
-        DEPENDS ${PRE_CONFIGURE_FILE}
-        BYPRODUCTS ${POST_CONFIGURE_FILE}
-        COMMENT "Checking the git repository for changes..."
-        COMMAND
-            ${CMAKE_COMMAND}
-            -D_BUILD_TIME_CHECK_GIT=TRUE
-            -DGIT_WORKING_DIR=${GIT_WORKING_DIR}
-            -DGIT_EXECUTABLE=${GIT_EXECUTABLE}
-            -DGIT_STATE_FILE=${GIT_STATE_FILE}
-            -DPRE_CONFIGURE_FILE=${PRE_CONFIGURE_FILE}
-            -DPOST_CONFIGURE_FILE=${POST_CONFIGURE_FILE}
-			-DSF_VERSION_MAJOR=${SF_VERSION_MAJOR}
-			-DSF_VERSION_MINOR=${SF_VERSION_MINOR}
-			-DSF_VERSION_PATCH=${SF_VERSION_PATCH}
-			-DORIGINAL_SOURCE_FOLDER=${CMAKE_SOURCE_DIR}
-            -P "${CMAKE_CURRENT_LIST_FILE}")
-endfunction()
-
-
-
-# Function: Main
-# Description: primary entry-point to the script. Functions are selected based
-#              on whether it's configure or build time.
-function(Main)
-    if(_BUILD_TIME_CHECK_GIT)
-        # Check if the repo has changed.
-        # If so, run the change action.
-        CheckGit("${GIT_WORKING_DIR}" did_change state)
-		LIST(GET state 3 GIT_VERSION)
-		string(REGEX REPLACE "^([0-9]+)\\..*" "\\1" GIT_VERSION_MAJOR "${GIT_VERSION}")
-		string(REGEX REPLACE "^[0-9]+\\.([0-9]+)\\..*" "\\1" GIT_VERSION_MINOR "${GIT_VERSION}")
-		string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+)-?.*" "\\1" GIT_VERSION_PATCH "${GIT_VERSION}")
-		string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.[0-9]+-?(.*)$" "\\1" GIT_VERSION_COMMIT "${GIT_VERSION}")
-		if (DEFINED GIT_VERSION_MAJOR AND NOT SF_VERSION_MAJOR STREQUAL GIT_VERSION_MAJOR)
-			message(SEND_ERROR "Correct major version (${SF_VERSION_MAJOR}->${GIT_VERSION_MAJOR}) in the CMakeList.txt")
-		endif()
-		if (DEFINED GIT_VERSION_MINOR AND NOT SF_VERSION_MINOR STREQUAL GIT_VERSION_MINOR)
-			message(SEND_ERROR "Correct minor version (${SF_VERSION_MINOR}->${GIT_VERSION_MINOR}) in the CMakeList.txt")
-		endif()
-		if (DEFINED GIT_VERSION_PATCH AND NOT SF_VERSION_PATCH STREQUAL GIT_VERSION_PATCH)
-			message(SEND_ERROR "Correct patch version (${SF_VERSION_PATCH}->${GIT_VERSION_PATCH}) in the CMakeList.txt")
-		endif()
-        if(did_change)
-            GitStateChangedAction("${state}")
-        endif()
-    else()
-        # >> Executes at configure time.
-        SetupGitMonitoring()
-    endif()
-endfunction()
-
-# And off we go...
-Main()
