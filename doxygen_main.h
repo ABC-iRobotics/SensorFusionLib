6dc2badd5201b7f5f6b79b84e6392167d82d684e
kj <kutij6@gmail.com>
2020-02-26 14:15:42 +0100

    Network setup example

--- doxygen_main.h
+++ doxygen_main.h
@@ -6,20 +6,39 @@
  *
  * The source contains CMake building system, tested on windows (Win10 x64), and linux (ubuntu x64).
  *
- * \section intro_sec Installation
+ * \section lib_install Library build and install
  *
- * - Clone the repository
+ * The simplest script to config & build & install from the source folder:
  *
- * - Run CMake
- *    - enable test, set policies for the third parties (easiest way: policy for third parties - download all), disable plot, set install folder
- *    - configure & generate
+ *	cmake -B ../build -DBUILD_ZMQ_TESTS:BOOL=FALSE -DPolicy_ALL_3RD_PARTIES="Download all" -DWITH_DOCS=FALSE .
  *
- * - Build the makefile/solution (cmake --build . from the build folder)
+ *	cd ../build
  *
- * - Run ctest (ctest .)
+ *	cmake --build .
  *
- * - Install package: (cmake --install .)
+ *	ctest .
  *
+ *	cmake --install .
+ *
+ * in CMake-gui you can see all of the availabe options
+ *
+ * \section project_install Create your own project(s)
+ *
+ * The CMakeList of your project must contain "find_package(SensorFusion)", see the example_app folder in the library source.
+ *
+ * You may have to specify the path to the package - by default it is the build/install folder, but it is relocatable
+ *
+ * \section setup_network Setup network
+ *
+ * An example architecture and corresponding setups a program codes are shown in the next figure:
+ *
+ * \htmlonly
+ * <embed src="network_setup_example.pdf" width="1080px" height="1000px" href="network_setup_example.pdf"></embed>
+ * \endhtmlonly
+ *
+ */
+ 
+ /*
  * \section usage Usage
  *
  * \subsection peripheries Peripheries
