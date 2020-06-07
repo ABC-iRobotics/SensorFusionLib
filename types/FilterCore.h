10d3331eead4f299cdc1396a0c19f9bc19cb029a
Jozsef Kuti <kutij6@gmail.com>
2020-06-07 19:05:44 +0200

    FilterCore class added

---
+++ types/FilterCore.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include"DataMsg.h"
+#include"defs.h"
+
+namespace SF {
+
+	/*! \brief Prototype of core of the filters
+	*
+	*
+	*
+	*/
+	class FilterCore {
+	public:
+		typedef std::shared_ptr<FilterCore> FilterCorePtr;
+
+		virtual void SaveDataMsg(const DataMsg& msg, const Time& currentTime) = 0; /*!< Must called if new DataMsg recieved */
+
+		virtual void SamplingTimeOver(const Time& currentTime) = 0; /*!< Is called in each sampling time - input: time */
+
+		virtual void MsgQueueEmpty(const Time& currentTime) = 0; /*!< Is called if the DataMsgs in the queue were read */
+
+		// TODO: getters
+	};
+}
