0d88959a6bb84311020b8651e7600986765e9a42
Jozsef Kuti <kutij6@gmail.com>
2021-05-26 15:22:50 +0200

    Tester for RelaxedUTN-based prediction

---
+++ tests/test_systemmanager.cpp
@@ -0,0 +1,208 @@
+#include "common/unity.h"
+void setUp() {}
+void tearDown() {}
+
+#include <thread>
+#include <iostream>
+#include"SystemManager.h"
+
+using namespace SF;
+
+
+class SystemManagerTester : public SystemManager {
+public:
+	SystemManagerTester(const BaseSystemData& data, const StatisticValue& state_) : SystemManager(data,state_) {}
+
+	void SamplingTimeOver(const Time& currentTime) override {}; /*!< Is called in each sampling time - input: time */
+
+	void MsgQueueEmpty(const Time& currentTime) override {}; /*!< Is called if the DataMsgs in the queue were read */
+
+};
+
+class TestBaseSystem : public BaseSystem {
+public:
+	TestBaseSystem() : BaseSystem(0) {}
+
+	Eigen::MatrixXd getA(double Ts) const {
+		Eigen::MatrixXd out = Eigen::MatrixXd::Identity(6,6);
+		out(2, 2) = 0;
+		out(3, 3) = 0;
+		out(3, 2) = 1;
+		out(5, 5) = 0;
+		return out;
+	}
+
+	Eigen::MatrixXd getB(double Ts) const {
+		Eigen::MatrixXd out = Eigen::MatrixXd::Zero(6, 3);
+		out(0, 0) = 1;
+		out(1, 1) = 1;
+		return out;
+	}
+
+	Eigen::MatrixXd getC(double Ts) const {
+		return Eigen::MatrixXd(0, 6);
+	}
+
+	Eigen::MatrixXd getD(double Ts) const {
+		return Eigen::MatrixXd(0, 3);
+	}
+
+	// The nonlinear parts and the dependencies are by default zeros
+	Eigen::VectorXi getStateUpdateNonlinXDep() const {
+		Eigen::VectorXi out = Eigen::VectorXi::Zero(6);
+		out[2] = 1;
+		out[3] = 1;
+		out[5] = 1;
+		return out;
+	}
+
+	Eigen::VectorXi getStateUpdateNonlinWDep() const {
+		Eigen::VectorXi out(3);
+		out[0] = 1;
+		out[1] = 1;
+		out[2] = 0;
+		return out;
+	}
+
+	Eigen::VectorXd EvalStateUpdateNonlinearPart(double Ts,
+		const Eigen::VectorXd& state, const Eigen::VectorXd& disturbance) const {
+		Eigen::VectorXd out(6);
+		out[0] = -disturbance[0];
+		out[1] = -disturbance[1];
+		out[2] = state[2];
+		out[3] = -state[2] + state[3];
+		out[4] = 0;
+		out[5] = state[5];
+		return out;
+	}
+
+	unsigned int getNumOfStates() const { return 6; }
+
+	unsigned int getNumOfDisturbances() const { return 3; }
+
+	unsigned int getNumOfOutputs() const { return 0;  }
+
+	unsigned int getNumOfNoises() const { return 0; }
+
+};
+
+
+/*
+
+#include "Filter.h"
+
+void orderDataMsg(std::string senderaddress, std::string recvaddress, int N) {
+	error = false;
+	msgs = std::vector<DataMsg>();
+	for (int i = 0; i < N; i++) {
+		DataMsg d(5, DataType(i % 4), OperationType(i % 3), Now());
+		if (i%3==0)
+			d.SetValueVector(Eigen::VectorXd::Ones(4)*i);
+		if (i % 2 == 0)
+			d.SetVarianceMatrix(Eigen::MatrixXd::Ones(i%6,i%6)*i);
+		msgs.push_back(d);
+	}
+	Forwarder a;
+	a.SetZMQOutput(senderaddress.c_str(), N);
+	auto checker = std::make_shared<Checker>();
+	Filter r(checker);
+	r.AddPeriphery(Filter::PeripheryProperties(recvaddress, true));
+	r.Start(DTime(1000));
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+	for (int i = 0; i < N; i++)
+		a.ForwardDataMsg(msgs[i],Now());
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+	while (r.GetNumOfRecievedMsgs(0) != N)
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	r.Stop();
+	if (error)
+		TEST_ASSERT(0);
+}
+
+void orderStrings(std::string senderaddress, std::string recvaddress, int N) {
+	error = false;
+	strings = std::vector<std::string>();
+	for (int i = 0; i < N; i++)
+		strings.push_back(std::string("asdassv0") + std::to_string(i) + "_" + std::to_string(duration_cast(Now().time_since_epoch()).count()));
+	Forwarder a;
+	a.SetZMQOutput(senderaddress.c_str(), N);
+	auto checker = std::make_shared<Checker>();
+	Filter r(checker);
+	r.AddPeriphery(Filter::PeripheryProperties(recvaddress, true));
+	r.Start(DTime(1000));
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+	for (int i = 0; i < N; i++)
+		a.ForwardString(strings[i],Now());
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+	while (r.GetNumOfRecievedMsgs(0) != N)
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	r.Stop();
+	if (error)
+		TEST_ASSERT(0);
+}*/
+
+int main (void) {
+	Eigen::MatrixXd Sw = Eigen::MatrixXd::Zero(3, 3);
+	Sw(0, 0) = 5;
+	Sw(1, 1) = 10;
+	Sw(1, 1) = 7;
+
+	StatisticValue in(Eigen::VectorXd::Zero(3), Sw);
+
+	SystemManagerTester::BaseSystemData bsData(std::make_shared<TestBaseSystem>(), StatisticValue(0),
+		in);
+
+	Eigen::MatrixXd Sx = Eigen::MatrixXd::Identity(6, 6);
+
+	StatisticValue state(Eigen::VectorXd::Zero(6), Sx);
+
+	SystemManagerTester tester(bsData, state);
+
+	StatisticValue y;
+	Eigen::MatrixXd Syin,Syx;
+
+	y = tester.Eval(TimeUpdateType::STATE_UPDATE, 0.001, state, in, Syx, Syin);
+
+	std::cout << y << std::endl << std::endl;
+	std::cout << Syx << std::endl << std::endl;
+	std::cout << Syin << std::endl << std::endl;
+	/*
+	UNITY_BEGIN();
+	RUN_TEST([]() { orderDataMsg("tcp://*:1234", "tcp://localhost:1234", 100); });
+	RUN_TEST([]() { orderStrings("tcp://*:1234", "tcp://localhost:1234", 100); });
+	RUN_TEST([]() {
+		printf("TCP: 1000x5 datamsg\n");
+		SendAndRecieveDataMsgs("tcp://*:1234", "tcp://localhost:1234", 1000, 5);
+		printf("TCP: 1000x5 string\n");
+		SendAndRecieveDataMsgs("tcp://*:1234", "tcp://localhost:1234", 1000, 5, true);
+		printf("TCP: 10000x5 datamsg\n");
+		SendAndRecieveDataMsgs("tcp://*:1234", "tcp://localhost:1234", 10000, 5);
+		printf("TCP: 10000x5 string\n");
+		SendAndRecieveDataMsgs("tcp://*:1234", "tcp://localhost:1234", 10000, 5, true);
+	});
+	RUN_TEST([]() {	hwmtest("tcp://*:1234", "tcp://localhost:1234", 10, 10); });
+	RUN_TEST([]() {	hwmtest("tcp://*:1234", "tcp://localhost:1234", 10, 100); });
+	RUN_TEST([]() {	DataMsgContentSerialization(100000); });
+	
+#ifdef UNIX
+	//ipc, inproc...
+	RUN_TEST([]() {
+		printf("1000,5, datamsg\n");
+		SendAndRecieveDataMsgs("tcp://*:1234", "tcp://localhost:1234", 1000, 5);
+		printf("1000,5, string\n");
+		SendAndRecieveDataMsgs("tcp://*:1234", "tcp://localhost:1234", 1000, 5, true);
+		printf("10000,5\n");
+		SendAndRecieveDataMsgs("tcp://*:1234", "tcp://localhost:1234", 10000, 5);
+		printf("10000,5, string\n");
+		SendAndRecieveDataMsgs("tcp://*:1234", "tcp://localhost:1234", 10000, 5, true);
+	});
+#endif
+*/
+	return UNITY_END();
+}
